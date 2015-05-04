083c17d5ce09adec349b637cbb3b1c356d79f39d
ANMOL MOHANTY <anmolnehru@gmail.com>
2015-05-04 12:03:25 -0500

    friend, objects of other classes

---
+++ Cpp/buckys/birthday.h
@@ -0,0 +1,17 @@
+#ifndef BIRTHDAY_H
+#define BIRTHDAY_H
+
+
+class birthday
+{
+    public:
+        birthday(int m,int d, int y);
+        void printdate();
+    protected:
+    private:
+        int m;
+        int d;
+        int y;
+};
+
+#endif // BIRTHDAY_H
---
+++ Cpp/buckys/people.h
@@ -0,0 +1,19 @@
+#ifndef PEOPLE_H
+#define PEOPLE_H
+#include <string>
+#include "birthday.h" //needed to store birthday object in this class
+
+using namespace std;
+
+class people
+{
+    public:
+        people(string x, birthday bo);
+        void dateprint();
+    protected:
+    private:
+    string name;
+    birthday dob; //borthday object
+};
+
+#endif // PEOPLE_H
