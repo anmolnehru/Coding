9bfabd60f3f8d1eb6435e78aadcf5c832a038aa1
Anmol Mohanty <anmolnehru@gmail.com>
2015-09-27 20:51:37 -0500

    remove linked list

---
+++ C/remove duplicates linkedlist.c
@@ -0,0 +1,107 @@
+//code to remove duplicates in linked list
+
+#include<stdlib.h>
+#include<stdio.h>
+
+//structure for the ll
+typedef struct Node{
+	int data;
+	struct Node* next;
+} Node;
+
+void remove_duplicates(Node *head);
+
+
+#include<stdio.h>
+int main()
+{
+	Node *current=(Node *)malloc(sizeof(Node));
+	Node *head;
+
+	Node *prev;
+	current->next=NULL;
+	head=current;
+
+	puts("Keep entering elements for your list. Enter -1 to terminate");
+
+	int temp;
+	scanf("%d",&temp);
+
+	while(temp!=-1)
+	{
+
+		prev=current;
+		current=(Node *)malloc(sizeof(Node));
+		prev->next=current;
+
+		//current node updation
+		current->data=temp;
+		current->next=NULL;
+		printf("Added another element succesfully\n");
+		scanf("%d",&temp);
+	}
+
+	printf("Before remove_duplicates the output is\n");
+
+	Node *print;
+	print=head;
+	while(print!=NULL)
+	{
+		printf("%d->",print->data);
+		print=print->next;
+	}
+
+
+    printf("IN the final stages of the loop \n");
+
+	remove_duplicates(head);
+
+
+	printf("Processing...\n");
+
+	printf("After remove_duplicates the output is\n");
+
+
+	print=head;
+	while(print!=NULL)
+	{
+		printf("%d->",print->data);
+		print=print->next;
+	}
+
+
+}
+
+
+void remove_duplicates(Node *head)
+{
+printf("Entered the remove_duplicates routine \n");
+
+Node *iter;
+iter=head;
+while(iter->next!=NULL)
+{
+    printf("Loopomg");
+    int	current_data=iter->data;
+	Node *temp_head;
+	temp_head=iter->next;
+    Node* prev=iter;
+	while(temp_head->next!=NULL)
+	{
+	    printf("Loopomg1");
+		if(current_data==temp_head->data){
+			prev->next=temp_head->next;
+			free(temp_head);
+			printf("Deleted something\n");
+		}
+		prev=temp_head;
+        temp_head=temp_head->next;
+			//free(temp_head);
+			//short_circuit(temp_head);
+	}
+}
+	iter=iter->next;
+
+}
+
+//void short_circuit(Node *input)
