8ac7230193d4d8234d2a9fd4f07763fed98ae4c3
Anmol Mohanty <anmolnehru@gmail.com>
2015-11-04 22:27:08 -0600

    2pac

---
+++ Cpp/sparse.cpp
@@ -0,0 +1,43 @@
+//sparse.cpp find the next sparse integer given an integer
+//sparse is no adjacent 1's in binary notation
+
+#include "iostream"
+//##include "sparse.h"
+
+using namespace std;
+
+int nextsparse(int n) //find the next sparse int given n;
+{
+	//binary representation of the int n-easy way
+	std::vector<bool> bin;
+
+	while(x!=0)
+	{
+		bin.push_back(x&1); //last bit of x is anded with 1
+		x>>1; //right shift x by 1 bit
+	}
+
+	//At the end of the loop, bin should have the binary rep of
+	//n as a vector
+	size_t length_bin = bin.size();
+
+	//logic to move it forward
+		int i=length_bin-1;  //last index pointed. We traverse from lsb
+			while(bni[i]&&bin[i-1]&&bin[i-2]==0)
+				{
+					
+
+
+
+
+
+		}
+	
+
+
+
+
+
+	// one life don't live with fear. shine bright like a diamond
+
+}
