229c2fb21358c8ef3ad63f2b7a8fb57f5a5c4f61
Anmol Mohanty <anmolnehru@gmail.com>
2015-11-07 18:40:58 -0600

    redblueredblue

--- C/redbluebluered.c
+++ C/redbluebluered.c
@@ -1,2 +1,27 @@
 //hackerrank coding challenge question:-
 
+/*
+ * Complete the function below.
+ */
+int wordpattern(string pattern, string input) {
+
+
+
+
+}
+
+int main() {
+    ofstream fout(getenv("OUTPUT_PATH"));
+    int res;
+    string _pattern;
+    getline(cin, _pattern);
+    
+    string _input;
+    getline(cin, _input);
+    
+    res = wordpattern(_pattern, _input);
+    fout << res << endl;
+    
+    fout.close();
+    return 0;
+}
